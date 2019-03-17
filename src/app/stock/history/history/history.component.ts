5a121dcf141d623bfc2a89b4718da2f1c05624c6
Daniel Cuesta <danicuesta00@gmail.com>
2019-03-17 18:38:00 +0100

    Historial de movimientos

--- src/app/stock/history/history/history.component.ts
+++ src/app/stock/history/history/history.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { StockService } from '../../stock.service';
 import { StockMovement } from '../../model/stock-movement';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-history',
@@ -10,13 +11,43 @@ import { StockMovement } from '../../model/stock-movement';
 export class HistoryComponent implements OnInit {
 
   movements: StockMovement[];
-  displayedColumns = ['date', 'reason', 'reference', 'name', 'size', 'color', 'quantity'];
+  displayedColumns = ['reason', 'order', 'reference', 'name', 'size', 'color', 'quantity'];
+  date: Date;
 
-  constructor(private stockService: StockService) { }
+  constructor(private stockService: StockService,
+              private router: Router,
+              private route: ActivatedRoute
+    ) { }
 
   ngOnInit() {
 
-    this.stockService.getMovementsByDate(new Date()).subscribe( (movements) => {
+    this.route.queryParams.subscribe( params => {
+      const selectedDate = (params && params.get) ? params.get('fecha') : undefined;
+      if (selectedDate) {
+        this.date = new Date(selectedDate);
+      } else {
+        this.date = new Date();
+      }
+      this.loadMovements();
+    });
+  }
+
+  previousDay() {
+    const newDate = new Date(this.date);
+    newDate.setDate(this.date.getDate() - 1);
+    this.date = newDate;
+    this.loadMovements();
+  }
+
+  nextDay() {
+    const newDate = new Date(this.date);
+    newDate.setDate(this.date.getDate() + 1);
+    this.date = newDate;
+    this.loadMovements();
+  }
+
+  private loadMovements() {
+    this.stockService.getMovementsByDate(this.date).subscribe( (movements) => {
       this.movements = movements;
     } );
   }
